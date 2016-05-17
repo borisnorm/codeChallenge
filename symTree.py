4c2009db12194e6aa4956a31e92fa83db8402b0f
ryanhang92@gmail.com <ryanhang92@gmail.com>
2016-05-17 14:43:47 -0700

    update with sym tree

---
+++ symTree.py
@@ -0,0 +1,25 @@
+Check if a tree is symetric
+
+  function symCheck(root):
+    leftTree =
+    rightTree =
+
+  leftDFS(node, array):
+    leftDFS(node.left. array)
+    array.append(node)
+    leftDFS(node.right), array
+
+  rightDFS(node, array):
+    rightDFS(node.right, array)
+    array.append(node)
+    leftDFS(node.left, array)
+
+
+
+
+
+
+
+
+
+
