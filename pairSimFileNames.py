f16f7df51136cdfb39c16d5edb40d40a225137ea
ryanhang92@gmail.com <ryanhang92@gmail.com>
2016-06-11 21:24:03 -0700

    adding asynch questions

---
+++ pairSimFileNames.py
@@ -0,0 +1,6 @@
+#Lets say that you had a list of 10k file names in a array
+#return an array of arrays that groups them togeather
+
+
+
+
---
+++ selfRefCycle.py
@@ -0,0 +1,18 @@
+#Say you have an array which stores values as indicies
+#where if the 1st item of the array, holds a value 2
+#The global array knows that value to be 3
+
+#Find if there is a cycle and if the longest cycle,
+#in terms of visitng stuff, is shorter or the same
+#as the length of the array
+
+
+
+
+
+
+
+
+
+
+
